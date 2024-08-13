ef097af250f56d0115256f60a2d7335bb3c884e3
degleg <legoff.d@outlook.com>
2024-08-13 18:18:45 +0200

    Set up and style header navigation and footer

--- src/components/organisms/Footer.js
+++ src/components/organisms/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
+import logo from "../../images/logo-medium-white.png";
+
 const Footer = () => {
-    return (
-        <div>
-            <p>Footer goes here</p>
-        </div>
-    )
-}
- 
-export default Footer
+  return (
+    <div className="footer">
+      <img className="footer__logo" src={logo} alt="Logo de Kasa" />
+      <p className="footer__text">© 2020 Kasa. All rights reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
