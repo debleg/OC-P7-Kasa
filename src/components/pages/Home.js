4870eeb514ad9f96754cec6a4e8ad82ecdfe8cb4
degleg <legoff.d@outlook.com>
2024-08-13 23:46:08 +0200

    Create and set up banner component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Banner from "../atoms/Banner"
+import HomeBanner from "../../images/home-banner-img.png"
+
+const homeBannerText = "Chez vous, partout et ailleurs"
 const Home = () => {
     return (
-        <>
-        <h1>
-            Titre de la page principale
-        </h1>
-        </>
+        <div className="homepage">
+        <Banner bannerImg = {HomeBanner} bannerText = {homeBannerText}/>
+        </div>
     )
 }
 
