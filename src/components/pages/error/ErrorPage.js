4b8c6d1d2706b20ea1ffa18e4f72054b9257b04d
degleg <legoff.d@outlook.com>
2024-08-17 00:03:27 +0200

    Build and style error page

--- src/components/pages/error/ErrorPage.js
+++ src/components/pages/error/ErrorPage.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
+const errorTypeText = ["Oups! La page que", <br key="line-break"/>, " vous demandez n'existe pas."];
+const redirectLinkText = "Retourner sur la page d'accueil"
+
     return (
-        <div>
-            <h1>404</h1>
+        <div className="errorpage">
+            <h1 className="errorpage__errortype">404</h1>
+            <p className="errorpage__errormessage">{errorTypeText}</p>
+            <Link to="/" className="errorpage__redirectlink">{redirectLinkText}</Link>
         </div>
     )
 }
