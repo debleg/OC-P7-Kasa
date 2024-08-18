bb889c9ae91a8cec1be91a8c917c13b446222bdc
degleg <legoff.d@outlook.com>
2024-08-19 01:00:21 +0200

    Complete About page with Collapse component

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -1,14 +1,47 @@
 import React from "react";
 import Banner from "../../atoms/banner/Banner";
-import AboutBanner from "../../../images/about-banner-img.png"
+import AboutBanner from "../../../images/about-banner-img.png";
+import Collapse from "../../molecules/collapse/Collapse";
 
 const About = () => {
-    return (
-      <div className="aboutpage">
-        <Banner bannerImg={AboutBanner} bannerImgBrightness="0.4"/>
+  const aboutContent = [
+    {
+      title: "Fiabilité",
+      text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      title: "Respect",
+      text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Service",
+      text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Sécurité",
+      text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
 
+  return (
+    <div className="aboutpage">
+      <Banner bannerImg={AboutBanner} bannerImgBrightness="0.7" />
+      <div className="aboutpage__content">
+        <Collapse title={aboutContent[0]["title"]}>
+          {aboutContent[0]["text"]}
+        </Collapse>
+        <Collapse title={aboutContent[1]["title"]}>
+          {aboutContent[1]["text"]}
+        </Collapse>
+        <Collapse title={aboutContent[2]["title"]}>
+          {aboutContent[2]["text"]}
+        </Collapse>
+        <Collapse title={aboutContent[3]["title"]}>
+          {aboutContent[3]["text"]}
+        </Collapse>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
-  export default About
+export default About;
